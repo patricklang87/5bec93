80126d5b92e30a308749b1ee0e48e5294d559d01
Patrick <patricklang87@gmail.com>
2022-01-21 16:41:30 -0500

    sort messages

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
 const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
+	const { messages, otherUser, userId } = props;
 
-  return (
-    <Box>
-      {messages.map((message) => {
-        const time = moment(message.createdAt).format("h:mm");
+	const sortedMessages = useMemo(() => {
+		return messages.sort((a, b) => {
+			return a.createdAt > b.createdAt;
+		});
+	}, [messages]);
 
-        return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
-        ) : (
-          <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
-        );
-      })}
-    </Box>
-  );
+	return (
+		<Box>
+			{sortedMessages.map((message) => {
+				const time = moment(message.createdAt).format("h:mm");
+
+				return message.senderId === userId ? (
+					<SenderBubble key={message.id} text={message.text} time={time} />
+				) : (
+					<OtherUserBubble
+						key={message.id}
+						text={message.text}
+						time={time}
+						otherUser={otherUser}
+					/>
+				);
+			})}
+		</Box>
+	);
 };
 
 export default Messages;
