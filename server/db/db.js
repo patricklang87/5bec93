e3b12156fc16f2adbc61660ac53c03d3d39b0b57
Patrick <patricklang87@gmail.com>
2022-01-21 13:38:05 -0500

    add async await to postMessage

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,10 @@
 const Sequelize = require("sequelize");
+require('dotenv').config();
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+const { PG_USER, PG_PASSWORD, PG_HOST, PG_PORT, PG_DATABASE } = process.env;
+const devConnectionString = `postgresql://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${PG_DATABASE}`;
+
+const db = new Sequelize(process.env.DATABASE_URL || devConnectionString, {
   logging: false
 });
 
