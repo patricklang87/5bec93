ad1529947a27aa1ccf0cd92750eb83bfb5556973
Patrick <patricklang87@gmail.com>
2022-01-28 16:56:52 -0500

    improve message count display

--- client/src/components/Sidebar/NewMessageCount.js
+++ client/src/components/Sidebar/NewMessageCount.js
@@ -1,33 +1,31 @@
 import { makeStyles } from "@material-ui/core/styles";
 import { Box, Typography } from "@material-ui/core";
 
-
-
 const useStyles = makeStyles(() => ({
-  root: {
-    display: "flex",
-    textAlign: "center",
-    marginRight: "20px",
-  },
-  text: {
-    padding: "2px 7px",
-    background: "#3F92FF",
-    borderRadius: "50%",
-    fontFamily: "Open Sans",
-    fontSize: "10px",
-    fontWeight: 700,
-    lineHeight: "14px",
-    color: "#FFFFFF",
-  }
+	root: {
+		display: "flex",
+		marginRight: "20px",
+		height: "20px",
+	},
+	text: {
+		padding: "2px 7px",
+		background: "#3F92FF",
+		borderRadius: "10px",
+		fontSize: "10px",
+		fontWeight: 700,
+		color: "#FFFFFF",
+	},
 }));
 
 const NewMessageCount = (props) => {
-  const classes = useStyles();
+	const classes = useStyles();
 	const { conversation } = props;
 
 	return (
 		<Box className={classes.root}>
-			<Typography className={classes.text}>{conversation.unreadMsgs}</Typography>
+			<Typography className={classes.text}>
+				{conversation.unreadMsgs}
+			</Typography>
 		</Box>
 	);
 };
